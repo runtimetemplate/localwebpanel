725252e6c7aaf7c13d1c37edbf37194f74c54fbb
Aiolos <47620043+iamjohnpalencia@users.noreply.github.com>
2020-08-03 09:54:22 +0800

    Commit
    Commit

---
+++ client/dashboard.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Innovention</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- This  -->
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css" > 
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+<?php include_once('templates/nav.php');?>
+<?php include_once('templates/sidenav.php');?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>150</h3>
+                <p>New Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                <p>Bounce Rate</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p>User Registrations</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Unique Visitors</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+  
+
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+  </div>
+  <!-- /.content-wrapper -->
+      <?php include_once('templates/webfooter.php');?>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<script src="../plugins/jquery/jquery.min.js"></script>
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../plugins/summernote/summernote-bs4.min.js"></script>
+<script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="../dist/js/adminlte.js"></script>
+<script src="../dist/js/demo.js"></script>
+<script>
+$(document).ready(function(){
+  $(".sidebar  a").click(function(){
+    $('.sidebar a').removeClass('active');
+    $(this).addClass('active');
+  })
+});
+</script>
+</body>
+</html>
+    
---
+++ client/index.php
@@ -0,0 +1,45 @@
+<?php session_start();
+
+  if(!isset($_SESSION["clientpanel"])) {
+    header("Location:../");
+  } else {
+    if ($_SERVER['REQUEST_URI'] == "/localwebpanel/client/index.php" || $_SERVER['REQUEST_URI'] == "/localwebpanel/client/index.php?dashboard" || $_SERVER['REQUEST_URI'] == "/localwebpanel/client/") 
+    {
+        include('dashboard.php');
+    }     
+    if(isset($_GET['outlets'])) {
+        include("outlets.php");
+    }
+    if(isset($_GET['users'])) {
+        include("users.php");
+    }
+    if(isset($_GET['settings'])) {
+        include("generalsettings.php");
+    }
+    if(isset($_GET['defprod'])) {
+        include("defproducts.php");
+    }
+    if(isset($_GET['defcat'])) {
+        include("defcategory.php");
+    }
+    if(isset($_GET['defform'])) {
+        include("defformula.php");
+    }
+    if(isset($_GET['definv'])) {
+        include("definventory.php");
+    }
+    if(isset($_GET['pmanagement'])) {
+        include("pmanagement.php");
+    }
+  }
+?>
+<script type="text/javascript">
+  function logout() {
+    $.ajax({  
+      url:"actions/logout.php",  
+      success:function(data){  
+       window.location = "../";
+      }  
+    });  
+  }
+</script>
